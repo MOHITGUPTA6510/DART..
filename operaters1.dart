523c4a4a147c87398fb61e60e10ebfd245a83758
MOHIT GUPTA <110682589+MOHITGUPTA6510@users.noreply.github.com>
2025-01-19 18:12:54 +0530

    Add files via upload

---
+++ operaters1.dart
@@ -0,0 +1,15 @@
+void main() {
+  //arithematic operator
+  int a1 = 2 + 3;
+  int a2 = 2 - 3;
+  int a3 = 2 * 3;
+  double a4 = 6 / 2;
+  int a5 = 2 ~/ 3;
+  int a6 = 5 % 2;
+  print(a1);
+  print(a2);
+  print(a3);
+  print(a4);
+  print(a5);
+  print(a6);
+}
