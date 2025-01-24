c227118ffd96cdbbcf56a5c5529f3f6d89faa52f
MOHIT GUPTA <110682589+MOHITGUPTA6510@users.noreply.github.com>
2025-01-24 17:45:09 +0530

    Add files via upload

---
+++ operator7.dart
@@ -0,0 +1,14 @@
+//Bitwise and Shift operator
+void main() {
+  int a = 5;
+  int b = 3;
+  print(a & b);
+  print(a | b);
+  print(a ^ b);
+  print(~a);
+  print(a << 1);
+  print(a << 2);
+  print(b >> 1);
+  print(b >> 2);
+  print(a >>> 1);
+}
